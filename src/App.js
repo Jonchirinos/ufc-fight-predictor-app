6724b1bbdd6a58e29162cd20ec86e4340099b4a9
Jon Chirinos <chirinosjon@gmail.com>
2022-07-18 20:50:10 -0500

    putting on my react hat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import { Provider } from "react-redux";
+import { ApolloClient, ApolloProvider, createHttpLink } from "@apollo/client";
+
+const httpLink = createHttpLink({
+    // uri: "/graphql",
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <Provider store={store}>
+                    {/* <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <p>
+                            Edit <code>src/App.js</code> and save to reload.
+                        </p>
+                        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                            Learn React
+                        </a>
+                    </header>
+                </div> */}
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </Provider>
+            </Router>
+        </ApolloProvider>
+    );
 }
 
 export default App;
